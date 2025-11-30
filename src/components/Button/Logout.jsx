6bf7cc59f45316e3c63b242ef44d744b7e6eb194
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-01 00:39:35 +0600

    fix

--- src/components/Button/Logout.jsx
+++ src/components/Button/Logout.jsx
@@ -1,8 +1,20 @@
+'use client'
+import axios from 'axios'
 import React from 'react'
 
 const Logout = () => {
+
+    const handleLogout= async () => {
+        try {
+            const response = await axios.get("/api/user/logout", {withCredentials: true})
+            alert(response.data.message)
+            window.location.replace('/login')
+        } catch (error) {
+            console.log(error)
+        }
+    }
   return (
-    <button className='cursor-pointer'>Logout</button>
+    <button className='cursor-pointer' onClick={handleLogout}>Logout</button>
   )
 }
 
