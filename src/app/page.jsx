3b4e2a56a003f5905a3b1731e866c80a2163fe41
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-02 00:41:58 +0600

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,10 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full min-h-screen flex flex-col items-center justify-center gap-2'>
-      <h1 className='text-xl font-semibold italic'>Welcome to Managers HomePage</h1>
+      <div className='w-full min-h-[700px] flex-col flex items-center justify-center gap-5'>
+        <h1>Welcome to</h1>
+        <h1>Monihari</h1>
+      </div>
       <Featured/>
     </div>
   )
