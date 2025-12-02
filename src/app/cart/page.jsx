e9ba9201240d40287fa97a500007674178f055a0
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-03 00:22:32 +0600

    update

--- src/app/cart/page.jsx
+++ src/app/cart/page.jsx
@@ -22,16 +22,25 @@ const Saved = () => {
     fetchData()
   }, [])
 
-  
+
   return (
     <div className="w-full min-h-screen flex items-center gap-4 flex-col p-2">
       {
-        data !== null ? <div className='w-full md:w-3/4 flex flex-col items-center min-h-screen'>
-
+        data !== null ? <div className='w-full md:w-1/2 flex flex-col items-center min-h-screen gap-6 bg-slate-50 p-4'>
+          <h1 className="text-2xl font-semibold">View Cart Item</h1>
+          <div className="w-full flex flex-row items-center justify-between gap-6 bg-slate-200 p-2">
+                <p className="w-full">Title</p>
+                <p>Quantity</p>
+                <p>Price</p>
+                <p>Action</p>
+              </div>
           {
             data.map((product) => (
-              <div key={product.productId}>
-                <p>{product.title}</p>
+              <div key={product.productId} className="w-full flex flex-row items-center justify-between gap-6 bg-slate-200 p-2">
+                <p className="w-full">{product.title}</p>
+                <p>{product.quantity}</p>
+                <p>{product.price}</p>
+                <p>Delete</p>
               </div>
             ))
           }
