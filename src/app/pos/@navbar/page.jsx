99fb8b70a3cf4d13d485b97bb8b45b2237b65f0b
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-23 23:43:43 +0600

    pos navbar

--- src/app/pos/@navbar/page.jsx
+++ src/app/pos/@navbar/page.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full border-b-2 h-14 border-black/10'>
+    <div className='w-full border-b-2 h-14 border-black/10 flex flex-row items-center justify-between px-4'>
+        <h1>Restaurant</h1>
+        <input type="text" placeholder='search item' className='border-2 outline-none border-black/10 px-3 p-1 rounded-xl'/>
+        <div className='flex flex-row items-center justify-center gap-4 h-14'>
+            <h1>Name</h1>
+            <p>Profile</p>
+            <p>Notification</p>
+        </div>
     </div>
   )
 }
